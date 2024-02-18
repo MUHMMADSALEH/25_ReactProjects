e933160e124413dd8565d9f3d046ab2bd69f59d0
MUHMMAD SALEH <khanmohdsaleh2002@gmail.com>
2024-02-18 12:11:42 +0530

    added new project Tree-view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
-import './App.css';
+import "./App.css";
+import TreeView from "./component/Tree-view";
+import menus from "./component/Tree-view/data";
 // import ImageSlider from './component/imageSlider';
-import LoadMoreData from './component/load-more-data';
+// import LoadMoreData from './component/load-more-data';
 // import RandomeColorGenerator from './component/randome_color_generator';
 // import StarRating from './component/star-rating';
 // import Accordian from './component/accordian';
@@ -9,11 +10,12 @@ import LoadMoreData from './component/load-more-data';
 function App() {
   return (
     <div className="App">
-     {/* <Accordian/> */}
-     {/* <RandomeColorGenerator/> */}
-     {/* <StarRating noOfStar={10}/> */}
-     {/* <ImageSlider/> */}
-     <LoadMoreData/>
+      {/* <Accordian/> */}
+      {/* <RandomeColorGenerator/> */}
+      {/* <StarRating noOfStar={10}/> */}
+      {/* <ImageSlider/> */}
+      {/* <LoadMoreData/> */}
+      <TreeView menus={menus} />
     </div>
   );
 }
---
+++ src/component/Tree-view/menu-item.js
@@ -0,0 +1,27 @@
+import MenuList from "./menu-list";
+import {useState} from "react"
+
+export default function MenuItem({ item }) {
+  const[displayCurrentChildren,setDisplayCurrentChildren]=useState({})
+   
+    function handleToggleChildren(getCurrentlabel){
+setDisplayCurrentChildren({...displayCurrentChildren,[getCurrentlabel]:!displayCurrentChildren[getCurrentlabel]})
+// console.log(!displayCurrentChildren[getCurrentlabel])
+    }
+    console.log(displayCurrentChildren)
+  return (
+    <li >
+      <div   className="item-container">
+      {
+        item && item.children && item.children.length?<span onClick={()=>handleToggleChildren(item.label)}>{
+          displayCurrentChildren[item.label]?"-":"+"
+        }</span>:null
+      }
+      <p>{item.label}</p>
+      </div>
+      {item && item.children && item.children.length>0  && displayCurrentChildren[item.label]?   (
+        <MenuList list={item.children} />
+      ) : null}
+    </li>
+  );
+}
